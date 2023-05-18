adac22ffe9eb9a449be112dc1940bea7038bf820
emiliaCaroDev <mariaemiliacarodh@gmail.com>
2023-05-18 17:48:14 -0300

    agregando ejercicios - Desarrollo en Java

---
+++ Ejercicios_Desarrollo _en _Java/Ejercicio_1/src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*====================== Ejercicio 1 =====================================*/
+
+        /*
+        String apellido = "Gomez";
+        int edad = 35;
+        boolean b = false;
+        double sueldo = 45857.90;
+        String nombre = "Julián";
+        */
+
+        /*====================== Ejercicio 2 =====================================*/
+
+
+        String nombre;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licenciaDeConducir;
+        double estaturaDeLaPersona;
+        int cantidadDeHijos;
+
+
+
+        /*====================== Ejercicio 3 =====================================*/
+
+
+        int cantidadDias = 15;
+        double precioPorDia = 2;
+        double costoTotalCampania =  precioPorDia * cantidadDias;
+
+
+        
+        /*====================== Ejercicio 3 =====================================*/
+
+        double premioTotal =23563899;
+        double impuesto1= premioTotal * 0.245;
+        double impuesto2= premioTotal * 0.15;
+        double impuesto3= premioTotal * 0.3;
+
+        double montoTotalNeto = premioTotal - impuesto1 - impuesto2 - impuesto3;
+
+    }
+}
